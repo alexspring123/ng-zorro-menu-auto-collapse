99ada3bae0401ec5f2639fb3f865fdfc30210ec3
alex <alex>
2019-01-05 14:44:04 +0800

    init

--- README.md
+++ README.md
@@ -1,27 +1,19 @@
 # NgZorroMenuAutoCollapse
+主要用来实现类似阿里云控制台鼠标摸上去自动显示菜单功能。
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.1.
+![Demo](https://github.com/alexspring123/ng-zorro-menu-auto-collapse/image/Demo.gif)
 
-## Development server
+## 主要设计方案
+1. 记录鼠标是否在主菜单上（mouseOnMenu），通过鼠标mouseover和mouseout事件实现
+2. 记录二级菜单的打开状态（subMenuState），并通过subMenu的nzOpen属性进行双向绑定
+3. 当发生如下场景时触发主菜单的刷新
+- 鼠标进入主菜单：展开主菜单
+- 鼠标离开主菜单：如果所有子菜单都关闭，则收缩主菜单，否则展开主菜单
+- 子菜单关闭时：如果鼠标在主菜单上，则展开主菜单；如果鼠标不在主菜单上，并且所有子菜单都关闭，则收缩主菜单
+- 子菜单打开：此场景不需要处理，因为只有鼠标在主菜单上时才会触发，与场景1相同
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+## 注意点
+1. 必须用鼠标的mouseover和mouseout事件来判断是否进入主菜单（注意不能使用mouseenter和mouseleave，因为不能冒泡）
+2. 由于主菜单和子菜单中间存在间隙，因此使用了Rxjs的Observer进行了防抖
+3. 修改了ant-menu的动画时间（从0.2s改成了0.1s）,否则会出现二级菜单先出来，而主菜单还没有完全展开的情况
+4. 由于subMenu使用了cdk组件，因此submenu弹出菜单并不包含在menu的Html元素中，因此监听不到mouseover。尝试过在subMenu中的每个item上增加鼠标事件，但是碰到了一个问题，menuItem存在margin值，当鼠标移动到margin上时，会触发mouseout事件。
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-zorro-menu-auto-collapse';
+export class AppComponent implements OnInit {
+
+  isCollapsed = true; // 主菜单当前状态
+  subMenuState = {};  // 子菜单打开状态
+  debounceTime = 10;
+  menuWidthCss = '80px'; // 主菜单宽度
+  mouseOnMenu = false; // 鼠标是否在主菜单
+  collapseChangeObserver: Observer<string>;
+
+  ngOnInit(): void {
+    this.subMenuState['subMenu1'] = false;
+    this.subMenuState['subMenu2'] = false;
+
+    if (!this.collapseChangeObserver) {
+      Observable.create(observer => { this.collapseChangeObserver = observer; })
+        .pipe(debounceTime(this.debounceTime)) // wait 300ms after the last event before emitting last event
+        .pipe(distinctUntilChanged()) // only emit if value is different from previous value
+        .subscribe((value) => {
+          console.log('接收到value =' + value);
+          console.log('allSubMenuClosed = ' + this.allSubMenuClosed());
+          console.log('mouseOnMenu =' + this.mouseOnMenu);
+
+          // 有子菜单打开 或 鼠标在主菜单上，展开主菜单
+          if (!this.allSubMenuClosed() || this.mouseOnMenu) {
+            this.unCollapsed();
+          } else {
+            this.collapsed();
+          }
+        });
+    }
+  }
+
+  openChange(open) {
+    if (!open) { // 关闭子菜单时需要触发检查
+      this.collapseChangeObserver.next('submenu closed');
+    }
+  }
+
+  mouseOver(): void {
+    this.mouseOnMenu = true;
+    this.collapseChangeObserver.next('mouseOnMenu');
+  }
+
+  mouseOut(): void {
+    this.mouseOnMenu = false;
+    this.collapseChangeObserver.next('mouseOut');
+  }
+
+  private allSubMenuClosed() {
+    for (const key in this.subMenuState) {
+      if (this.subMenuState[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private collapsed() {
+    this.isCollapsed = true;
+    this.menuWidthCss = '80px';
+  }
+
+  private unCollapsed() {
+    this.isCollapsed = false;
+    this.menuWidthCss = '240px';
+  }
 }
